c78e552b9e5f2a8cc7e67d2f122e98e4e16b52d7
dilligaf911 <58417546+dilligaf911@users.noreply.github.com>
2024-09-16 00:15:13 +0500

    Update run.sh

--- frp-client/run.sh
+++ frp-client/run.sh
@@ -8,11 +8,11 @@ function stop_frpc() {
     kill -15 "${WAIT_PIDS[@]}"
 }
 
-bashio::log.info "Copying configuration."
-cp $DEFAULT_CONFIG_PATH $CONFIG_PATH
-sed -i "s/serverAddr = \"your_server_addr\"/serverAddr = \"$(bashio::config 'serverAddr')\"/" $CONFIG_PATH
-sed -i "s/serverPort = 7000/serverPort = $(bashio::config 'serverPort')/" $CONFIG_PATH
-sed -i "s/auth.token = \"123456789\"/auth.token = \"$(bashio::config 'authToken')\"/" $CONFIG_PATH
+#bashio::log.info "Copying configuration."
+#cp $DEFAULT_CONFIG_PATH $CONFIG_PATH
+#sed -i "s/serverAddr = \"your_server_addr\"/serverAddr = \"$(bashio::config 'serverAddr')\"/" $CONFIG_PATH
+#sed -i "s/serverPort = 7000/serverPort = $(bashio::config 'serverPort')/" $CONFIG_PATH
+#sed -i "s/auth.token = \"123456789\"/auth.token = \"$(bashio::config 'authToken')\"/" $CONFIG_PATH
 #sed -i "s/webServer.port = 7500/webServer.port = $(bashio::config 'webServerPort')/" $CONFIG_PATH
 #sed -i "s/webServer.user = \"admin\"/webServer.user = \"$(bashio::config 'webServerUser')\"/" $CONFIG_PATH
 #sed -i "s/webServer.password = \"123456789\"/webServer.password = \"$(bashio::config 'webServerPassword')\"/" $CONFIG_PATH
